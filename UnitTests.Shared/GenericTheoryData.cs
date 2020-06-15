1174c1057ada500863355f7565ae49d36a6c4252
Lewis Grint <51135182+bearandhammer@users.noreply.github.com>
2020-06-15 21:47:05 +0100

    Added UnitTests.Shared.
    Contains some helper types to more easily work with theory data.

---
+++ UnitTests.Shared/GenericTheoryData.cs
@@ -0,0 +1,21 @@
+﻿namespace UnitTests.Shared
+{
+    /// <summary>
+    /// Represents a generic piece of input/expected output
+    /// theory data 'pairs'.
+    /// </summary>
+    /// <typeparam name="TInput">The input.</typeparam>
+    /// <typeparam name="TExpectation">The expected output.</typeparam>
+    public class GenericTheoryData<TInput, TExpectation> : TheoryData
+    {
+        /// <summary>
+        /// Adds input/expected output pairs.
+        /// </summary>
+        /// <param name="input">The input.</param>
+        /// <param name="expectedValue">The expected output.</param>
+        public void Add(TInput input, TExpectation expectedValue)
+        {
+            AddRow(input, expectedValue);
+        }
+    }
+}
