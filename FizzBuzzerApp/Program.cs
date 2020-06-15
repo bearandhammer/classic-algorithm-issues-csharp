772bc76ea3fd3d2b08affa3416c21c9afedca7ca
Lewis Grint <51135182+bearandhammer@users.noreply.github.com>
2020-06-15 18:16:55 +0100

    Initial commit.

---
+++ FizzBuzzerApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FizzBuzzerApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
