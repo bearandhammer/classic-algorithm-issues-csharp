6d131117fcbdc03b60e6f7a8f5b07254dce3308e
Lewis Grint <51135182+bearandhammer@users.noreply.github.com>
2020-06-15 22:05:03 +0100

    Failing tests added.
    No CICD to pick this up as it stands, so included now so I can fix up next.

--- FizzBuzzerApp.Tests/FizzBuzzerTests.cs
+++ FizzBuzzerApp.Tests/FizzBuzzerTests.cs
@@ -1,12 +1,18 @@
-﻿using Xunit;
+﻿using FizzBuzzerApp.AppTypes;
+using FizzBuzzerApp.Tests.TestData;
+using FluentAssertions;
+using Xunit;
 
 namespace FizzBuzzerApp.Tests
 {
     public class FizzBuzzerTests
     {
-        [Fact]
-        public void Test1()
+        [Theory()]
+        [ClassData(typeof(FizzBuzzTestData))]
+        public void FizzBuzz_Inputs_CorrectedTranslated(int inputValue, string expectedResult)
         {
+            // Arrange/act/assert
+            new FizzBuzzer().FizzBuzz(inputValue).Should().Be(expectedResult);
         }
     }
 }
