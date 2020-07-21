5fb9462db3ce3ea46ffc8dc9953ef1c4b7772f09
Lewis Grint <51135182+bearandhammer@users.noreply.github.com>
2020-07-21 20:10:07 +0100

    Starter red light tests for IValidator.

--- FizzBuzzerApp.Tests/ValidatorTests.cs
+++ FizzBuzzerApp.Tests/ValidatorTests.cs
@@ -1,4 +1,7 @@
+using FizzBuzzerApp.AppTypes;
+using FizzBuzzerApp.Interfaces;
 using FizzBuzzerApp.Tests.TestData;
+using FluentAssertions;
 using Xunit;
 
 namespace FizzBuzzerApp.Tests
@@ -6,28 +9,42 @@ namespace FizzBuzzerApp.Tests
     public class ValidatorTests
     {
         [Fact]
-        public void TryParseInput_RawStartEndValues_NotIntegers_Returns_False()
+        public void TryParseInput_RawStartEndValues_NotBothIntegers_Returns_False()
         {
-            // TODO
+            // Arrange
+            IValidator validatorUnderTest = new Validator();
+            string intValue = "5", nonIntValue = "zero";
+
+            // Act/Assert
+            validatorUnderTest.TryParseInput(nonIntValue, intValue, out _).Should().BeFalse();
+            validatorUnderTest.TryParseInput(intValue, nonIntValue, out _).Should().BeFalse();
+            validatorUnderTest.TryParseInput(string.Empty, null, out _).Should().BeFalse();
+            validatorUnderTest.TryParseInput(null, string.Empty, out _).Should().BeFalse();
+            validatorUnderTest.TryParseInput(null, null, out _).Should().BeFalse();
+            validatorUnderTest.TryParseInput(string.Empty, string.Empty, out _).Should().BeFalse();
         }
 
         [Theory]
         [ClassData(typeof(OutOfRangeInputValues))]
         public void TryParseInput_RawStartEndValues_OutsideZeroToOneHundredRange_Returns_False((string RawStartValue, string RawEndValue) inputValues, bool expectedResult)
         {
-            // TODO
+            // Arrange/Act/Assert
+            new Validator().TryParseInput(inputValues.RawEndValue, inputValues.RawEndValue, out _).Should().Be(expectedResult);
         }
 
         [Fact]
         public void TryParseInput_RawStartEndValues_StartGreaterThanEnd_Returns_False()
         {
-            // TODO
+            new Validator().TryParseInput("2", "1", out _).Should().BeFalse();
         }
 
         [Fact]
         public void TryParseInput_RawStartEndValues_InRange_StartLessThanEnd_Returns_True()
         {
             // TODO
+            new Validator().TryParseInput("1", "2", out (int StartValue, int EndValue) inputValues).Should().BeTrue();
+            inputValues.StartValue.Should().Be(1);
+            inputValues.EndValue.Should().Be(2);
         }
     }
 }
